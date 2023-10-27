385ff9a3ea8e1220752d238039b6dc5d101bb05e
bribrioche <bryan.guillot@outlook.com>
2023-10-27 17:52:47 +0200

    deplacements + peche + inventaire

--- mainTest.js
+++ mainTest.js
@@ -6,9 +6,7 @@ import { Sky } from "three/addons/objects/Sky.js";
 import { FBXLoader } from "three/addons/loaders/FBXLoader.js";
 
 let camera, scene, renderer;
-let controls, water, sun, boat;
-// Créez un tableau pour stocker les sphères générées
-const spheres = [];
+let controls, water, sun;
 
 init();
 animate();
@@ -43,6 +41,7 @@ function init() {
 
   // Boat
   const fbxLoader = new FBXLoader();
+  let boat;
 
   fbxLoader.load(
     "./3d_models/boat.fbx",
@@ -194,34 +193,6 @@ function init() {
   const ambientLight = new THREE.AmbientLight(0xffffff, 3); // Couleur et intensité
   scene.add(ambientLight);
 
-  // Fonction pour créer une sphère noire
-  function createBlackSphere() {
-    if (boat) {
-      const geometry = new THREE.SphereGeometry(5, 32, 32);
-      const material = new THREE.MeshBasicMaterial({ color: 0x000000 });
-      const sphere = new THREE.Mesh(geometry, material);
-
-      // Positionnez la sphère à une position aléatoire autour du bateau
-      const radius = Math.random() * 50 + 50; // Rayon de génération
-      const angle = Math.random() * Math.PI * 2;
-      const x = boat.position.x + Math.cos(angle) * radius;
-      const z = boat.position.z + Math.sin(angle) * radius;
-      sphere.scale.set(0.6, 0.1, 0.2);
-      sphere.position.set(x, 0, z);
-      sphere.rotateY(Math.random() * 180);
-
-      scene.add(sphere);
-      spheres.push(sphere);
-      if (spheres.length > 5) {
-        scene.remove(spheres[0]);
-        spheres.shift();
-      }
-    }
-  }
-
-  createBlackSphere();
-  // Appelez la fonction pour créer une sphère toutes les 60 secondes (60000 ms)
-  setInterval(createBlackSphere, 10000);
   updatePosition();
 
   window.addEventListener("resize", onWindowResize);
@@ -237,8 +208,6 @@ function onWindowResize() {
 function animate() {
   requestAnimationFrame(animate);
   render();
-  updateSpheres();
-  detectCollision();
 }
 
 function render() {
@@ -249,7 +218,6 @@ function render() {
   renderer.render(scene, camera);
 }
 
-//PLay sounds
 function playWaterSound() {
   waterSound.volume = 0.2;
   waterSound.playbackRate = 1;
@@ -276,140 +244,3 @@ function stopBoatSound() {
   boatSound.pause();
   boatSound.currentTime = 0;
 }
-
-// Fonction pour mettre à jour les sphères existantes (par exemple, les supprimer si elles sont trop éloignées)
-function updateSpheres() {
-  const maxDistance = 1000; // Distance maximale avant de supprimer une sphère
-
-  for (let i = spheres.length - 1; i >= 0; i--) {
-    const sphere = spheres[i];
-    const distance = boat.position.distanceTo(sphere.position);
-
-    if (distance > maxDistance) {
-      scene.remove(sphere);
-      spheres.splice(i, 1);
-    }
-  }
-}
-
-const messageElement = document.querySelector(".messageElement");
-
-function detectCollision() {
-  if (boat) {
-    const collisionDistance = 10; // Distance de collision, ajustez selon vos besoins
-
-    // Parcourez toutes les sphères noires
-    for (let i = 0; i < spheres.length; i++) {
-      const sphere = spheres[i];
-      const distance = boat.position.distanceTo(sphere.position);
-
-      if (distance < collisionDistance) {
-        // Retirez la sphère de la scène et du tableau
-        scene.remove(sphere);
-        spheres.splice(i, 1);
-
-        // Affichez le message
-        messageElement.style.display = "flex";
-
-        // Masquez le message après 3 secondes
-        setTimeout(() => {
-          messageElement.style.display = "none";
-          messageElement.style.pointerEvents = "none";
-          startSpacebarChallenge();
-        }, 3000);
-
-        // Sortez de la boucle, car une seule collision est gérée à la fois
-        break;
-      }
-    }
-  }
-}
-
-function startSpacebarChallenge() {
-  progressContainer.style.display = "block";
-
-  const goalClicks = 20;
-  spacebarClickCount = 0;
-  spacebarChallengeCompleted = false;
-  progressBar.style.width = "0%";
-
-  document.addEventListener("keydown", (event) => {
-    if (event.key === " ") {
-      handleSpacebarClick();
-    }
-  });
-
-  setTimeout(() => {
-    if (!spacebarChallengeCompleted) {
-      progressContainer.style.display = "none";
-    } else {
-      showCongratulationsMessage();
-    }
-  }, 10000); // Arrêtez le défi après 10 secondes
-}
-
-const progressContainer = document.querySelector(".progress-container");
-const progressBar = document.querySelector(".progress-bar");
-
-let spacebarClickCount = 0;
-let spacebarChallengeCompleted = false;
-
-// Fonction pour incrémenter le compteur de clics sur la barre d'espace
-function handleSpacebarClick() {
-  spacebarClickCount++;
-  updateProgressBar();
-}
-
-// Fonction pour mettre à jour la barre de progression
-function updateProgressBar() {
-  if (!spacebarChallengeCompleted) {
-    const goalClicks = 20;
-    const percentage = (spacebarClickCount / goalClicks) * 100;
-    progressBar.style.width = percentage + "%";
-
-    if (spacebarClickCount >= goalClicks) {
-      spacebarChallengeCompleted = true;
-      progressBar.style.width = "100%";
-      showCongratulationsMessage();
-    }
-  }
-}
-
-// Fonction pour afficher le message de félicitations
-function showCongratulationsMessage() {
-  messageElement.innerText = "Bravo !";
-  messageElement.style.display = "flex";
-
-  const randomString = generateRandomString();
-
-  // Ajouter la chaîne à la liste
-  const listContainer = document.getElementById("listContainer");
-  const listItem = document.createElement("h2");
-  listItem.textContent = randomString;
-  listContainer.appendChild(listItem);
-}
-
-// Fonction pour générer une chaîne aléatoire
-function generateRandomString() {
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  const length = 8; // Longueur de la chaîne aléatoire
-
-  let randomString = "";
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    randomString += characters.charAt(randomIndex);
-  }
-
-  return randomString;
-}
-
-// Sélectionnez le bouton et la liste
-const showListButton = document.getElementById("showListButton");
-const listContainer = document.getElementById("listContainer");
-
-// Ajoutez un gestionnaire d'événements pour le bouton
-showListButton.addEventListener("click", () => {
-  // Basculez la classe "hidden" sur la liste pour l'afficher ou la masquer
-  listContainer.classList.toggle("hidden");
-});
